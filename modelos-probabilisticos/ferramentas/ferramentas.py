3902b762c5e55d733b728928123c4652f0321567
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-26 11:32:17 -0300

    mais funcionalidades

--- modelos-probabilisticos/ferramentas/ferramentas.py
+++ modelos-probabilisticos/ferramentas/ferramentas.py
@@ -2,30 +2,69 @@ import math
 
 #recebe um array p/ calcular desvio padrão
 class Desvio:
+  
+  status = {}
+  ma = 0
+  somatoria = 0
+  dp = 0
+  total = 0
+  z = []
+  eiy = []
 
-    def __init__(self, arr):
-        self.arr = arr
-        self.ma = 0
-        self.somatoria = 0
-        self.dp = 0
-        self.total = 0
+  def __init__(self, arr):
+    self.arr = arr
+    
 
-    def Dp(self):
+  def Dp(self):
+    #obter a somatoria
+    for item in self.arr:
+      self.somatoria += item
 
-        #obter a somatoria
-        for item in self.arr:
-            self.somatoria += item
+    #obter a media aritimética
+    self.ma = self.somatoria / len(self.arr)
+    self.status['media'] = self.ma
+    self.status['somatoria'] = self.somatoria
 
-        #obter a media aritimética
-        self.ma = self.somatoria / len(self.arr)
+    #subtrair itens - media Arit elevado ao quadrado
+    for item in self.arr:
+      self.total += math.pow((item - self.ma), 2)
 
-        #subtrair itens - media Arit elevado ao quadrado
-        for item in self.arr:
-            self.total += math.pow((item - self.ma), 2)
+    #dividir resultado p/ quantidade de itens
+    self.total = self.total / len(self.arr)
+    self.status['total'] = self.total
+    #obter a raiz para obter desvio padrão
+    self.dp = math.sqrt(self.total)
+    self.status['desvio'] = self.dp
 
-        #dividir resultado p/ quantidade de itens
-        self.total = self.total / len(self.arr)
-        #obter a raiz para obter desvio padrão
-        self.dp = math.sqrt(self.total)
+    #distribuição normal padrão
+  def Score(self):
+    for item in self.arr:
+      self.z.append((item - self.ma)/self.dp)
+      
+    self.status['zcore'] = self.z  
+    #falta obter dados da tabela pnorm
+    #z = (x-u)/o
+
+  def Colunas(self):
+    for item in self.z:
+      if '-' in str(item):
+        self.eiy.append('0.0'+str(item)[4])
+      else:
+        print(item) 
+        self.eiy.append('0.0'+str(item)[3])
+
+    self.status['eixoY'] = self.eiy
+    
+
+  
+
+    
+    
+
+      
+
+
+
+
 
 
